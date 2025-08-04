77cb4b35312deb3f32851abc6f1f45affc5403e3
Enrique Moreno <emorenocode@gmail.com>
2025-08-03 20:50:55 -0500

    Event pattern - emit eventos

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, NATS_SERVICE } from 'src/config';
 
 @Module({
   controllers: [PaymentsController],
   providers: [PaymentsService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: NATS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ],
 })
 export class PaymentsModule {}
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Request, Response } from 'express';
-import { envs } from 'src/config';
+import { envs, NATS_SERVICE } from 'src/config';
 import { PaymentSessionDto } from 'src/payments/dto/payment-session.dto';
 import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
     const { currency, items, orderId } = paymentSessionDto;
@@ -64,10 +68,13 @@ export class PaymentsService {
       case 'charge.succeeded':
         // Llamar nuestro servicio
         const chargeSucceeded = event.data.object;
-        console.log(event);
-        console.log({
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
           orderId: chargeSucceeded.metadata.orderId,
-        });
+          receiptUrl: chargeSucceeded.receipt_url,
+        };
+        this.logger.log({ payload });
+        this.client.emit('payment.succeeded', payload);
         break;
       default:
         console.log(`Event ${event.type} not handled`);
